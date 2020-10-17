0e182cc2263e538a46c700af6ecbabdf93eb04a0
Jordan Ruetz <jordan.ruetz@mail.utoronto.ca>
2020-10-17 12:24:08 -0400

    get secret and connect to db

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,44 @@
-var express = require("express");
-var http = require("http");
-var path = require("path");
+import express from "express"
+import http from "http"
+import path from "path"
+import typeorm from "typeorm"
+import {getSecret} from "./fetchSecret.js"
+import { fileURLToPath } from 'url';
+
+import student from "./entity/student.json"
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const startServer = async () => {
+  const secret = await getSecret();
+  const jsonSecret = JSON.parse(secret)
+
+  var EntitySchema = typeorm.EntitySchema
+
+  const connection = await typeorm.createConnection({ 
+     "type": jsonSecret.engine, 
+     "host": jsonSecret.host, 
+     "port": jsonSecret.port, 
+     "username": jsonSecret.username, 
+     "password": jsonSecret.password, 
+     "database": "dbmaster",
+     "synchronize": true, 
+     "logging": false,
+     entities: [ new EntitySchema(student) ] 
+  })
+
+//   const repo = connection.getRepository("Student")
+//   var student = { 
+//     name: "Student1", 
+//     age: 18 
+//  };
+//  repo.save(student)
+}
+
+startServer()
+
+
 
 var app = express();
 
